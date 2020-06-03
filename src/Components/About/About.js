438944150b4450fda496bec421987453117d54ac
kelar crisp <kelcrisp007@icloud.com>
2020-06-03 12:48:14 -0700

    adding a small feature for the about page

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -5,17 +5,27 @@ import Typewriter from 'typewriter-effect';
 const About = () => {
 
     const [text, setText] = useState('')
+    const { themeColor } = useContext(ThemeContext)
 
 
-    let bio;
     useEffect(() => {
+        window.addEventListener('scroll', e => {
+
+            const target = document.querySelector('.circle')
+
+            var posX = window.pageYOffset * 2;
+            var posY = window.pageYOffset * 1;
+            target.style.transform = 'translate3d(' + posX + 'px, ' + posY + 'px, 0px)'
+
+
+        })
         setTimeout(() => {
             setText(<div><p className={classes.Bio}>Hello! I'm Kelar, a Seattle based software engineer with a passion for all things front-end. I currently spend most of my time building things with React 🤖</p>
                 <p className={classes.Bio}>  </p></div>)
         }, 1500)
     }, [])
 
-    const { themeColor } = useContext(ThemeContext)
+
     console.log(themeColor)
     return (
         <div id='About' className={themeColor ? classes.DarkContainer : classes.MainContainer}>
@@ -24,7 +34,7 @@ const About = () => {
                 {/* <h1 className={classes.Title}> Front End developer</h1> */}
                 {text}
 
-
+                <span data-ratey="1" data-ratex="2" data-direction="horizontal" className='circle'></span>
             </div>
         </div>
     );
