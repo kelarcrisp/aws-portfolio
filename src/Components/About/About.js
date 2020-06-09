28d82e33e9c7364767290eb8c03e4f2f40e858b2
kelar crisp <kelcrisp007@icloud.com>
2020-06-09 16:26:14 -0700

    took out the circle

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,43 +1,39 @@
-import React, { useContext, useEffect, useState } from 'react';
-import classes from './About.module.css';
-import { ThemeContext } from '../../Shared/contexts/ThemeContext';
-import Typewriter from 'typewriter-effect';
+import React, { useContext, useEffect, useState } from "react";
+import classes from "./About.module.css";
+import { ThemeContext } from "../../Shared/contexts/ThemeContext";
+import Typewriter from "typewriter-effect";
 const About = () => {
+  const [text, setText] = useState("");
+  const { themeColor } = useContext(ThemeContext);
 
-    const [text, setText] = useState('')
-    const { themeColor } = useContext(ThemeContext)
-
-
-    useEffect(() => {
-        window.addEventListener('scroll', e => {
-
-            const target = document.querySelector('.circle')
-
-            var posX = window.pageYOffset * 2;
-            var posY = window.pageYOffset * 1;
-            target.style.transform = 'translate3d(' + posX + 'px, ' + posY + 'px, 0px)'
-
-
-        })
-        setTimeout(() => {
-            setText(<div><p className={classes.Bio}>Hello! I'm Kelar, a Seattle based software engineer with a passion for all things front-end. I currently spend most of my time building things with React 🤖</p>
-                <p className={classes.Bio}>  </p></div>)
-        }, 1500)
-    }, [])
-
-
-    console.log(themeColor)
-    return (
-        <div id='About' className={themeColor ? classes.DarkContainer : classes.MainContainer}>
-            <div className={classes.AboutCardContainer}>
-                <h1 className={classes.Name}>Kelar Crisp </h1>
-                {/* <h1 className={classes.Title}> Front End developer</h1> */}
-                {text}
-
-                <span data-ratey="1" data-ratex="2" data-direction="horizontal" className='circle'></span>
-            </div>
+  useEffect(() => {
+    setTimeout(() => {
+      setText(
+        <div>
+          <p className={classes.Bio}>
+            Hello! I'm Kelar, a Seattle based software engineer with a passion
+            for all things front-end. I currently spend most of my time building
+            things with React 🤖
+          </p>
+          <p className={classes.Bio}> </p>
         </div>
-    );
+      );
+    }, 1500);
+  }, []);
+
+  console.log(themeColor);
+  return (
+    <div
+      id="About"
+      className={themeColor ? classes.DarkContainer : classes.MainContainer}
+    >
+      <div className={classes.AboutCardContainer}>
+        <h1 className={classes.Name}>Kelar Crisp </h1>
+        {/* <h1 className={classes.Title}> Front End developer</h1> */}
+        {text}
+      </div>
+    </div>
+  );
 };
 
 export default About;
