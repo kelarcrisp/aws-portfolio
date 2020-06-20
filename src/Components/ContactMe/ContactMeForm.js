859b3b1ac6d637d1639dd4eacccab41c984e8e82
kelar crisp <kelcrisp007@icloud.com>
2020-06-20 14:28:16 -0700

    added emailjs

--- src/Components/ContactMe/ContactMeForm.js
+++ src/Components/ContactMe/ContactMeForm.js
@@ -1,129 +1,207 @@
-import React, { useState, useContext } from 'react';
-import TextField from '@material-ui/core/TextField';
-import classes from './ContactMeForm.module.css';
-import Button from '@material-ui/core/Button';
-import SendIcon from '@material-ui/icons/Send';
-import axios from 'axios';
-import { ThemeContext } from '../../Shared/contexts/ThemeContext';
-import AOS from 'aos';
-import 'aos/dist/aos.css';
-const ContactMeForm = (props) => {
+import React, { useState, useContext } from "react";
+import TextField from "@material-ui/core/TextField";
+import classes from "./ContactMeForm.module.css";
+import Button from "@material-ui/core/Button";
+import SendIcon from "@material-ui/icons/Send";
+import axios from "axios";
+import { ThemeContext } from "../../Shared/contexts/ThemeContext";
+import AOS from "aos";
+import emailjs from "emailjs-com";
+import "aos/dist/aos.css";
 
-    const { themeColor } = useContext(ThemeContext)
-    const [email, setEmail] = useState('');
-    const [title, setTitle] = useState('');
-    const [message, setMessage] = useState('');
-    const [isSent, setIsSent] = useState();
+const ContactMeForm = props => {
+  const { themeColor } = useContext(ThemeContext);
+  const [email, setEmail] = useState("");
+  const [title, setTitle] = useState("");
+  const [message, setMessage] = useState("");
+  const [isSent, setIsSent] = useState();
 
-    AOS.init();
+  AOS.init();
 
+  const handleChange = e => {
+    e.preventDefault();
+    if (e.target.id === "email") {
+      setEmail(e.target.value);
+    } else if (e.target.id === "title") {
+      setTitle(e.target.value);
+    } else {
+      setMessage(e.target.value);
+    }
+  };
 
-    const handleChange = (e) => {
-        e.preventDefault();
-        if (e.target.id === 'email') {
-            setEmail(e.target.value)
-        } else if (e.target.id === 'title') {
-            setTitle(e.target.value)
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    const emailToSend = {
+      from_name: email,
+      to_name: "kelcrisp007@icloud.com",
+      subject: title,
+      message_html: message
+    };
+    const serviceId = "gmail";
+    const templateId = "template_KBdWeh9k";
+    const userId = "user_dyzjhIcsAc5ZmpmGA2Kif";
+    emailjs
+      .send(serviceId, templateId, emailToSend, userId)
+      .then(res => "woo")
+      .catch(err => console.log(err, "err"));
+
+    const dataToSubmit = {
+      email: email,
+      title: title,
+      message: message
+    };
+    setIsSent(true);
+    setEmail("");
+    setTitle("");
+    setMessage("");
+    axios
+      .post(
+        "https://kelar-portfolio.firebaseio.com/formData.json",
+        dataToSubmit
+      )
+      .then(response => console.log(response, "its been sent"));
+    // axios.post('/api/form', dataToSubmit).then(() => console.log('sent to server!')
+    // ).catch(err => err, 'request couldnt be sent!')
+    console.log("data sent", dataToSubmit);
+  };
+
+  let showSentMessage = null;
+  if (isSent) {
+    showSentMessage = "Your email has successfully been sent!";
+  }
+  if (isSent) {
+    setTimeout(() => {
+      setIsSent(null);
+    }, 2500);
+  }
+
+  return (
+    <div
+      id="ContactMe"
+      data-aos="fade-right"
+      className={
+        themeColor ? classes.DarkContactContainer : classes.ContactContainer
+      }
+    >
+      <div
+        className={
+          themeColor ? classes.DarkContactHeader : classes.ContactHeader
         }
-        else {
-            setMessage(e.target.value)
-        }
-    }
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        const dataToSubmit = {
-            email: email,
-            title: title,
-            message: message
-        }
-        setIsSent(true);
-        setEmail('');
-        setTitle('');
-        setMessage('');
-        axios.post('https://kelar-portfolio.firebaseio.com/formData.json', dataToSubmit).then(response => console.log(response, 'its been sent'))
-        // axios.post('/api/form', dataToSubmit).then(() => console.log('sent to server!')
-        // ).catch(err => err, 'request couldnt be sent!')
-        console.log('data sent', dataToSubmit)
-    }
-
-    let showSentMessage = null;
-    if (isSent) {
-        showSentMessage = 'Your email has successfully been sent!'
-    }
-    if (isSent) {
-        setTimeout(() => {
-            setIsSent(null)
-        }, 2500)
-    }
-
-    return (
-        <div id='ContactMe' data-aos='fade-right' className={themeColor ? classes.DarkContactContainer : classes.ContactContainer} >
-            <div className={themeColor ? classes.DarkContactHeader : classes.ContactHeader}>Fill out the form to directly contact me by Email!</div>
-            <form
-                onSubmit={handleSubmit}
-                className={classes.FormInfoContainer} autoComplete="on">
-                <TextField className={classes.TextField}
-                    InputProps={themeColor ? {
-                        style: {
-                            color: "#ddd",
-                        }
-                    } : null}
-                    InputLabelProps={themeColor ? {
-                        style: {
-                            color: "#ddd",
-                        }
-                    } : null}
-                    type='email' id="email" label="Your Email " variant="outlined" value={email} onChange={handleChange} />
-                <TextField className={classes.TextField}
-                    InputProps={themeColor ? {
-                        style: {
-                            color: "#ddd",
-                        }
-                    } : null}
-                    InputLabelProps={themeColor ? {
-                        style: {
-                            color: "#ddd",
-                        }
-                    } : null}
-                    id="title" label="Title" variant="outlined" value={title} onChange={handleChange} />`
-                    <TextField
-                    className={classes.TextField}
-                    InputProps={themeColor ? {
-                        style: {
-                            color: "#ddd",
-                        }
-                    } : null}
-                    onChange={handleChange}
-                    multiline
-                    rows='4'
-
-                    InputProps={{
-                        style: {
-                            color: "#ddd",
-                        }
-                    }}
-
-                    InputLabelProps={themeColor ? {
-                        style: {
-                            color: "#ddd",
-                        }
-                    } : null}
-                    id="message" label='Message' variant="outlined" value={message} />
-                <Button
-                    onClick={handleSubmit}
-                    variant="contained"
-                    color="primary"
-                    style={{ marginTop: '50px', marginLeft: '-10px' }}
-                    startIcon={<SendIcon></SendIcon>}
-                >
-                    Send!
-                    </Button>
-                <div className={!themeColor ? classes.ShowSentMessage : classes.ShowDarkSentMessage}>{showSentMessage}</div>
-            </form>
-        </div >
-    );
-
+      >
+        Fill out the form to directly contact me by Email!
+      </div>
+      <form
+        onSubmit={handleSubmit}
+        className={classes.FormInfoContainer}
+        autoComplete="on"
+      >
+        <TextField
+          className={classes.TextField}
+          InputProps={
+            themeColor
+              ? {
+                  style: {
+                    color: "#ddd"
+                  }
+                }
+              : null
+          }
+          InputLabelProps={
+            themeColor
+              ? {
+                  style: {
+                    color: "#ddd"
+                  }
+                }
+              : null
+          }
+          type="email"
+          id="email"
+          label="Your Email "
+          variant="outlined"
+          value={email}
+          onChange={handleChange}
+        />
+        <TextField
+          className={classes.TextField}
+          InputProps={
+            themeColor
+              ? {
+                  style: {
+                    color: "#ddd"
+                  }
+                }
+              : null
+          }
+          InputLabelProps={
+            themeColor
+              ? {
+                  style: {
+                    color: "#ddd"
+                  }
+                }
+              : null
+          }
+          id="title"
+          label="Title"
+          variant="outlined"
+          value={title}
+          onChange={handleChange}
+        />
+        `
+        <TextField
+          className={classes.TextField}
+          InputProps={
+            themeColor
+              ? {
+                  style: {
+                    color: "#ddd"
+                  }
+                }
+              : null
+          }
+          onChange={handleChange}
+          multiline
+          rows="4"
+          InputProps={{
+            style: {
+              color: "#ddd"
+            }
+          }}
+          InputLabelProps={
+            themeColor
+              ? {
+                  style: {
+                    color: "#ddd"
+                  }
+                }
+              : null
+          }
+          id="message"
+          label="Message"
+          variant="outlined"
+          value={message}
+        />
+        <Button
+          onClick={handleSubmit}
+          variant="contained"
+          color="primary"
+          style={{ marginTop: "50px", marginLeft: "-10px" }}
+          startIcon={<SendIcon></SendIcon>}
+        >
+          Send!
+        </Button>
+        <div
+          className={
+            !themeColor ? classes.ShowSentMessage : classes.ShowDarkSentMessage
+          }
+        >
+          {showSentMessage}
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default ContactMeForm;
