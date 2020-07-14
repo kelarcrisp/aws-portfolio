6c52023f929f86064fbc59a87ac456b786db2c7a
kelar crisp <kelcrisp007@icloud.com>
2020-07-14 12:14:28 -0700

    basic landingpage animation for logo

--- src/App.js
+++ src/App.js
@@ -1,84 +1,24 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
-import NavHeader from './Shared/NavHeader/NavHeader';
-import HomePage from './Components/HomePage/HomePage';
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
-import SideProjects from './Components/SideProjects/SideProjects'
-import About from './Components/About/About';
-import ContactForm from './Components/ContactMe/ContactMeForm';
-import MiniDrawer from './Shared/SideDrawer/SideDrawer';
-import { ThemeContext } from './Shared/contexts/ThemeContext';
-import * as THREE from 'three'
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import NavHeader from "./Shared/NavHeader/NavHeader";
+import HomePage from "./Components/HomePage/HomePage";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import SideProjects from "./Components/SideProjects/SideProjects";
+import About from "./Components/About/About";
+import ContactForm from "./Components/ContactMe/ContactMeForm";
+import MiniDrawer from "./Shared/SideDrawer/SideDrawer";
+import { ThemeContext } from "./Shared/contexts/ThemeContext";
+
 const App = () => {
+  const [themeColor, setThemeColor] = useState(false);
 
-  const [themeColor, setThemeColor] = useState(false)
-
-
-  useEffect(() => {
-    let scene, camera, renderer, starGeo, stars;
-    init();
-    animate();
-    function init() {
-      scene = new THREE.Scene();
-      camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 1, 1000);
-      camera.position.z = 1;
-      camera.rotation.x = Math.PI / 2
-      renderer = new THREE.WebGLRenderer();
-      renderer.setSize(window.innerWidth, window.innerHeight);
-      const element = document.getElementById('About')
-      element.classList.add('canvas')
-      element.append(renderer.domElement);
-
-
-
-
-      starGeo = new THREE.Geometry();
-      for (let i = 0; i < 6000; i++) {
-        let star = new THREE.Vector3(
-          Math.random() * 600 - 300,
-          Math.random() * 600 - 300,
-          Math.random() * 600 - 300,
-        );
-        star.velocity = 0;
-        star.acceleration = 0.02;
-        starGeo.vertices.push(star)
-      }
-      let image = require('./assets/images/star.png')
-      let sprite = new THREE.TextureLoader().load(image);
-      console.log(sprite)
-      let starMaterial = new THREE.PointsMaterial({
-        color: 0xaaaaaa,
-        size: 0.7,
-        map: sprite
-      })
-      stars = new THREE.Points(starGeo, starMaterial)
-      scene.add(stars)
-      animate();
-    }
-    function animate() {
-      starGeo.vertices.forEach((p) => {
-        p.velocity += p.acceleration;
-        p.y -= p.velocity;
-        if (p.y < -200) {
-          p.y = 200;
-          p.velocity = 0;
-        }
-      })
-      starGeo.verticesNeedUpdate = true;
-      stars.rotation.y += 0.002;
-      renderer.render(scene, camera)
-      requestAnimationFrame(animate)
-    }
-    init();
-
-  }, [])
   return (
     <BrowserRouter>
       <>
         <ThemeContext.Provider value={{ themeColor, setThemeColor }}>
           {/* <NavHeader /> */}
           {/* <MiniDrawer /> */}
-          <Route path='/' exact component={HomePage} />
+          <Route path="/" exact component={HomePage} />
           {/* <Route path='/Projects' component={SideProjects} />
             <Route path='/About' component={About} />
             <Route path='/ContactForm' component={ContactForm} /> */}
@@ -87,8 +27,7 @@ const App = () => {
         </ThemeContext.Provider>
       </>
     </BrowserRouter>
-
   );
-}
+};
 
 export default App;
--- src/Components/HomePage/HomePage.js
+++ src/Components/HomePage/HomePage.js
@@ -1,41 +1,28 @@
-import React, { Component } from 'react';
-import classes from './HomePage.module.css';
-import WorkInfoCard from './WorkInfoCard/WorkInfoCard'
-import { ThemeContext } from '../../Shared/contexts/ThemeContext';
-import SideProjects from '../SideProjects/SideProjects';
-import ContactMeForm from '../ContactMe/ContactMeForm';
-import About from '../About/About';
-class HomePage extends Component {
+import React, { useEffect, useState } from "react";
+import classes from "./HomePage.module.css";
+import WorkInfoCard from "./WorkInfoCard/WorkInfoCard";
+import { ThemeContext } from "../../Shared/contexts/ThemeContext";
+import SideProjects from "../SideProjects/SideProjects";
+import ContactMeForm from "../ContactMe/ContactMeForm";
+import LandingPageSvg from "../landingPageSvg/LandingPageSvg";
+import About from "../About/About";
+const HomePage = () => {
+  const [showAbout, setShowAbout] = useState(false);
+  useEffect(() => {
+    setTimeout(() => {
+      setShowAbout(true);
+    }, 5000);
+  }, []);
 
-
-    render() {
-        return (
-            <div >
-                <About />
-                <ThemeContext.Consumer>{(context) => {
-                    const { themeColor } = context
-
-                    let loadDropDown;
-
-                    if (localStorage.getItem('showDropDown') == null)
-                        loadDropDown = <div className={classes.HomePageContainer}>  <WorkInfoCard /></div>
-                    else if (localStorage.getItem('showDropDown') !== null)
-                        loadDropDown = <div className={classes.HomePageContainerTwo}> <WorkInfoCard /> </div>
-                    if (themeColor)
-                        loadDropDown = <div className={classes.DarkHomePageContainerTwo}> <WorkInfoCard /> </div>
-                    return (
-                        <div>
-                            {loadDropDown}
-                        </div>
-                    )
-                }}</ThemeContext.Consumer>
-                <SideProjects />
-                <ContactMeForm />
-            </div>
-        )
-    }
-
-
+  const showWhat = showAbout ? <About /> : <LandingPageSvg />;
+  return (
+    <div>
+      {showWhat}
+      <WorkInfoCard />
+      <SideProjects />
+      <ContactMeForm />
+    </div>
+  );
 };
 
 export default HomePage;
