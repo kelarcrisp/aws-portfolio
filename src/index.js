5a5db8ec1ba8b09b5e4655fbcb67534f51c7383c
kelar crisp <kelcrisp007@icloud.com>
2020-08-03 20:43:37 -0700

    deleted firebase config

--- src/index.js
+++ src/index.js
@@ -1,21 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import firebase from 'firebase'
-var firebaseConfig = {
-    apiKey: process.env.API_KEY,
-    authDomain: "kelar-portfolio.firebaseapp.com",
-    databaseURL: "https://kelar-portfolio.firebaseio.com",
-    projectId: "kelar-portfolio",
-    storageBucket: "kelar-portfolio.appspot.com",
-    messagingSenderId: process.env.MESSAGE_SENDER_ID,
-    appId: process.env.APP_ID,
-};
-
-firebase.initializeApp(firebaseConfig)
-ReactDOM.render(<App />, document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
